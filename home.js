1ea441ccd67668a72af64f8340b05ceb3de2c444
Umair Kifayat <136703449+umairkifayat@users.noreply.github.com>
2023-11-14 10:22:32 -0800

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,3 @@
-
-
-
-
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 import { auth, db } from "./config.js";
 import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
